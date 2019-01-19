5d295832805008f7a321844229866c15699108fb
Carla Voldemar <carlavoldemar@gmail.com>
2019-01-19 12:57:54 +0000

    Second Update

--- src/Empregado.java
+++ src/Empregado.java
@@ -1,4 +1,47 @@
 
 public class Empregado {
 
+	private String name;
+	private int age;
+	private double wage;
+	
+	
+	public Empregado(String name, int age, double wage) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.wage = wage;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public double getWage() {
+		return wage;
+	}
+
+
+	public void setWage(double wage) {
+		this.wage = wage;
+	}
+	
+	
 }
